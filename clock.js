7d0a1659ef2f7dd7d01830fdafc4f77845b865a3
Maik Sonnenberg <maik.sonnenberg@hotmail.de>
2023-08-10 12:54:02 +0200

    vorläufiges Ende

--- clock.js
+++ clock.js
@@ -5,6 +5,9 @@ let ss = document.getElementById('ss');
 let sec_dot = document.querySelector('.sec_dot');
 let min_dot = document.querySelector('.min_dot');
 let hr_dot = document.querySelector('.hr_dot');
+let hr =document.getElementById('hr');
+let mn =document.getElementById('mn');
+let sc =document.getElementById('sc');
 
 let h = new Date().getHours();
 let m = new Date().getMinutes();
@@ -16,10 +19,17 @@ hh.style.strokeDashoffset = 510 - (510 * h) / 12;
 mm.style.strokeDashoffset = 630 - (630 * m) / 60;
 // 60 sekunden
 ss.style.strokeDashoffset = 760 - (760 * s) / 60;
-// 360 grad
+// 360 grad / 60 sekunden = 6
 sec_dot.style.transform = `rotateZ(${s * 6}deg)`;
+// 360 grad / 60 minuten = 6
 min_dot.style.transform = `rotateZ(${m * 6}deg)`;
+// 360 grad / 12 stunden = 30
 hr_dot.style.transform = `rotateZ(${h * 30}deg)`;
+
+//zeiger bewegen
+hr.style.transform = `rotateZ(${h * 30}deg)`;
+mn.style.transform = `rotateZ(${m * 6}deg)`;
+sc.style.transform = `rotateZ(${s * 6}deg)`;
 
 })
 
